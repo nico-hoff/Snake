3872d390f86281951cb001f1a7083dbd1e77d1d2
Nicolai Hoff <nico.hoff@tum.de>
2021-01-06 23:27:57 +0100

    nico

--- src/src/GameBoard.java
+++ src/src/GameBoard.java
@@ -3,15 +3,25 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Arrays;
 
 public class GameBoard {
-	int[][] board = new int[100][100];
-
+	int[][] board;
 	public GameBoard() {
-		this.board = null;
+		this.board = setupBoard();
 	}
 
 	public static void main(String[] args) {
 
 	}
 
+	public void printBoard() {
+
+	}
+
+	public static int[][] setupBoard() {
+		int[][] board = new int[100][100];
+		Arrays.stream(board)
+			.forEach(c -> Arrays.stream(c)
+				.forEach(v -> v = 0));
+		return board;
+	}
 
 }
