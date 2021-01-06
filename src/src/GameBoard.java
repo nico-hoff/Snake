33b3eb847da37507d687fcca5a7bb8c412d5fe02
Nicolai Hoff <nico.hoff@tum.de>
2021-01-06 23:37:56 +0100

    nico

--- src/src/GameBoard.java
+++ src/src/GameBoard.java
@@ -2,8 +2,10 @@ import java.util.Arrays;
 
 public class GameBoard {
 	int[][] board;
+
 	public GameBoard() {
-		this.board = setupBoard();
+		//inport int (size)
+		this.board = setupBoard(size);
 	}
 
 	public static void main(String[] args) {
@@ -12,15 +14,16 @@ public class GameBoard {
 	}
 
 	public void printBoard() {
-		for(int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
-				System.out.println(board[i][j]);
+		for(int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j]);
 			}
+			System.out.println("");
 		}
 	}
 
-	public static int[][] setupBoard() {
-		int[][] board = new int[100][100];
+	public static int[][] setupBoard(int size) {
+		int[][] board = new int[size][size];
 		Arrays.stream(board)
 			.forEach(c -> Arrays.stream(c)
 				.forEach(v -> v = 0));
