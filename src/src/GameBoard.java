b9312bee1d80eb95b25693c9ad193b72fd89cea7
Nicolai Hoff <nico.hoff@tum.de>
2021-01-06 23:09:41 +0100

    nico

---
+++ src/src/GameBoard.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class GameBoard {
+	int[][] board = new int[100][100];
+
+	public GameBoard() {
+		this.board = null;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+
+}
